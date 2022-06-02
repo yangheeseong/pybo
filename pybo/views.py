ba1018064efabb377fecc2dff80f7771964c77c7
yangheeseong <hs.yang@theenm.com>
2022-06-02 18:45:52 +0900

    목록, 상세 페이지 생성

--- pybo/views.py
+++ pybo/views.py
@@ -1,7 +1,27 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+
+    return render(
+        request,
+        'pybo/question_list.html',
+        context
+    )
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    #question = Question.objects.get(id=question_id)
+    context = {'question': question}
+
+    return render(
+        request,
+        'pybo/question_detail.html',
+        context
+    )
+
