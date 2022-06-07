45e7c13860c027ad66d78816f98a2aa704b78616
yangheeseong <hs.yang@theenm.com>
2022-06-07 14:43:41 +0900

    회원가입 및 자동로그인 완료

--- common/views.py
+++ common/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from common.forms import Userform
 
 # Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = Userform(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+
+            return redirect('index')
+    else:
+        form = Userform()
+
+    return render(request, 'common/signup.html', {'form': form})
