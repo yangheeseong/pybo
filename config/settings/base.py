8a43d91c8edc9ccecb22baa15b5a3080e48cf4ac
yangheeseong <hs.yang@theenm.com>
2022-06-08 15:35:27 +0900

    settings.py 분리 완료

--- config/settings/base.py
+++ config/settings/base.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -121,7 +121,7 @@ USE_TZ = False
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
-]
+    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
