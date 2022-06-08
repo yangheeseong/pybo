5c907f045a42d3aa42dc4d66eee81a6213c8be92
yangheeseong <hs.yang@theenm.com>
2022-06-08 16:51:49 +0900

    디버그 모드 설정 완료

--- common/views.py
+++ common/views.py
@@ -18,3 +18,7 @@ def signup(request):
         form = Userform()
 
     return render(request, 'common/signup.html', {'form': form})
+
+
+def page_not_found(request, exception):
+    return render(request, 'common/404.html', {})
