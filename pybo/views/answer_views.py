1d1849b6a35408557caecb1a1e1ce23108eb9879
yangheeseong <hs.yang@theenm.com>
2022-06-08 12:29:45 +0900

    앵커, 검색 기능 추가 완료

--- pybo/views/answer_views.py
+++ pybo/views/answer_views.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseNotAllowed
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, resolve_url
 from django.utils import timezone
 
 from ..forms import AnswerForm
@@ -27,9 +27,12 @@ def answer_create(request, question_id):
             answer.question = question
             answer.save()
 
-            return redirect(
-                'pybo:detail',
-                question_id=question_id
+            # return redirect(
+            #     'pybo:detail',
+            #     question_id=question_id
+            # )
+            return redirect('{}#answer_{}'.format(
+                resolve_url('pybo:detail', question_id=question.id), answer.id)
             )
     else:
         return HttpResponseNotAllowed('Only POST is possible.')
@@ -50,7 +53,9 @@ def answer_modify(request, answer_id):
     if request.user != answer.author:
         messages.error(request, '수정권한이 없습니다.')
 
-        return redirect('pybo:detail', question_id=answer.question.id)
+        # return redirect('pybo:detail', question_id=answer.question.id)
+        return redirect('{}#answer_{}'.format(
+            resolve_url('pybo:detail', question_id=answer.question.id), answer.id))
 
     if request.method == 'POST':
         form = AnswerForm(request.POST, instance=answer)
@@ -60,8 +65,9 @@ def answer_modify(request, answer_id):
             answer.modify_date = timezone.now()
             answer.save()
 
-            return redirect('pybo:detail', question_id=answer.question.id)
-
+            # return redirect('pybo:detail', question_id=answer.question.id)
+            return redirect('{}#answer_{}'.format(
+                resolve_url('pybo:detail', question_id=answer.question.id), answer.id))
     else:
         form = AnswerForm(instance=answer)
 
@@ -91,4 +97,6 @@ def answer_vote(request, answer_id):
     else:
         answer.voter.add(request.user)
 
-    return redirect('pybo:detail', question_id=answer.question.id)
+    # return redirect('pybo:detail', question_id=answer.question.id)
+    return redirect('{}#answer_{}'.format(
+        resolve_url('pybo:detail', question_id=answer.question.id), answer.id))
