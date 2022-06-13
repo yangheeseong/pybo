a3e85a0f4a1d83bdac9c214cf36a24a5437769a1
yangheeseong <hs.yang@theenm.com>
2022-06-13 17:56:36 +0900

    [docker] Gunicorn 적용 완료

--- config/settings.py
+++ config/settings.py
@@ -128,9 +128,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-    ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,9 @@ from django.urls import path, include
 
 from pybo.views import base_views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')),
@@ -25,4 +28,5 @@ urlpatterns = [
     path('', base_views.index, name='index'),
 ]
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 handler404 = 'common.views.page_not_found'
